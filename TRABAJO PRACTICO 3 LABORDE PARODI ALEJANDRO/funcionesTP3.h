6b295e9f2421d6775f734a366b36d312ad1d5bfe
Alejandro Laborde Parodi <36174258+AlejandroLaborde@users.noreply.github.com>
2018-06-01 00:17:01 -0300

    Add files via upload

---
+++ TRABAJO PRACTICO 3 LABORDE PARODI ALEJANDRO/funcionesTP3.h
@@ -0,0 +1,48 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+
+typedef struct{
+    char titulo[50];
+    char genero[20];
+    int duracion;
+    char descripcion[500];
+    int puntaje;
+    char linkImagen[500];
+    int estado;
+}EMovie;
+
+
+/**
+ *  Agrega una pelicula al archivo binario
+ *  @param movie la estructura a ser agregada al archivo
+ *  @return retorna 1 o 0 de acuerdo a si pudo agregar la pelicula o no
+ */
+int agregarPelicula(EMovie pelicula, FILE * archivoBin);
+
+/**
+ *  Borra una pelicula del archivo binario
+ *  @param movie la estructura a ser eliminada al archivo
+ *  @return retorna 1 o 0 de acuerdo a si pudo eliminar la pelicula o no
+ */
+int borrarPelicula(FILE*archivoBin,EMovie movie);
+
+/**
+ *  Genera un archivo html a partir de las peliculas cargadas en el archivo binario.
+ *  @param lista la lista de peliculas a ser agregadas en el archivo.
+ *  @param nombre el nombre para el archivo.
+ */
+void generarPagina(FILE * archivoBin,EMovie pelicula);
+
+void imprimeCabezeraTXT(FILE *archivoTexto);
+void imprimeCuerpoTXT(FILE *archivoTexto,EMovie pelicula);
+void imprimeFinalTXT(FILE *archivoTexto);
+void imprimeLista(FILE*archivoBin,EMovie pelicula);
+int  modificaPelicula(FILE *archivoTexto,EMovie pelicula);
+
+void harcodeaPeliculas(EMovie pelicula, FILE *archivoBinario);
+
+void imprimeMenu();
+
+void imprime(char texto[1000]);
+#endif // FUNCIONES_H_INCLUDED
---
+++ TRABAJO PRACTICO 3 LABORDE PARODI ALEJANDRO/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "funcionesTP3.h"
+#define ARCHIVOBINARIO ".\\PeliculasBinario.dat"
+#define ARCHIVOTEXTO ".\\PeliculasTexto.txt"
+
+
+int main()
+{
+    char seguir='s';
+    int opcion=0;
+
+
+    FILE*archivoBin;
+    EMovie pelicula;
+
+  //abreArchivo(FILE *archivo)
+
+
+    if ((archivoBin=fopen(ARCHIVOBINARIO,"rb+"))==NULL)
+    {
+        if ((archivoBin=fopen(ARCHIVOBINARIO,"wb+"))==NULL)
+        {
+            printf("No se pudo abrir el archivo");
+            exit(1);
+        }
+    }
+
+    while(seguir=='s')
+    {
+        imprimeMenu();
+        scanf("%d",&opcion);
+
+        switch(opcion)
+        {
+            case 1:
+
+                agregarPelicula(pelicula, archivoBin);
+                break;
+
+            case 2:
+                borrarPelicula(archivoBin, pelicula);
+                break;
+            case 3:
+                modificaPelicula(archivoBin, pelicula);
+               break;
+            case 4:
+                generarPagina(archivoBin,pelicula);
+                break;
+            case 5:
+                seguir = 'n';
+                break;
+            case 9:
+                harcodeaPeliculas(pelicula,archivoBin);
+                break;
+
+        }
+    }
+
+
+
+    fclose(archivoBin);
+
+    return 0;
+}
