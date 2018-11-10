251b00995303943f9e5181fd78139fbad865afa4
Alejandro Laborde Parodi <36174258+AlejandroLaborde@users.noreply.github.com>
2018-11-10 11:11:58 -0300

    Add files via upload

---
+++ Trabajo Practico 3 LABORDE PARODI 2do cuatrimestre 2018/Employee.h
@@ -0,0 +1,33 @@
+#ifndef employee_H_INCLUDED
+#define employee_H_INCLUDED
+typedef struct
+{
+    int id;
+    char nombre[128];
+    int horasTrabajadas;
+    int sueldo;
+}Employee;
+
+Employee* employee_new();
+Employee* employee_newParametros(int * idStr,char* nombreStr,int * horasTrabajadasStr,int * sueldo);
+void employee_delete(Employee * this);
+int employee_sortNombre(void* empleado,void * empleado2);
+int employee_sortID(void* empleado,void * empleado2);
+int employee_sortHorasTrabajadas(void* empleado,void * empleado2);
+int employee_sortSueldo(void* empleado,void * empleado2);
+int employee_editEmploye(Employee* empleado);
+
+
+int employee_setId(Employee* list,int id);
+int employee_getId(Employee* list,int* id);
+
+int employee_setNombre(Employee* list,char* nombre);
+int employee_getNombre(Employee* list,char* nombre);
+
+int employee_setHorasTrabajadas(Employee* list,int horasTrabajadas);
+int employee_getHorasTrabajadas(Employee* list,int* horasTrabajadas);
+
+int employee_setSueldo(Employee* list,int sueldo);
+int employee_getSueldo(Employee* list,int* sueldo);
+
+#endif // employee_H_INCLUDED
---
+++ Trabajo Practico 3 LABORDE PARODI 2do cuatrimestre 2018/main.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Controller.h"
+#include "Employee.h"
+#include "FuncionesLPA.h"
+
+
+int main()
+{
+    int isOpen;
+    int option = 0;
+    LinkedList* listaEmpleados = ll_newLinkedList();
+
+    do{
+        MenuPrincipal();
+        getInt(&option,"Ingrese la opcion que desea seleccionar: ","El dato es incorrecto",1,10);
+        switch(option)
+        {
+            case 1:{
+                system("cls");
+                isOpen=controller_loadFromText("data.cvs",listaEmpleados);
+                if(isOpen==0){
+                    printf("Hubo un error al momento de abrir el archivo\n");
+                }else{
+                    isOpen=1;
+                    printf("Se abrio el archivo correctamente\n");
+                    }
+                system("pause");
+                break;
+                }
+            case 2:{
+                system("cls");
+                isOpen=controller_loadFromBinary("data.bin",listaEmpleados);
+                if(isOpen==0){
+                    printf("Hubo un error al momento de abrir el archivo\n");
+                }else{
+                    isOpen=1;
+                    printf("Se abrio el archivo correctamente\n");
+                    }
+                system("pause");
+                break;
+                }
+
+            case 3:
+                system("cls");
+                    controller_addEmployee(listaEmpleados);
+                system("pause");
+                break;
+            case 4:
+                system("cls");
+                    controller_editEmployee(listaEmpleados);
+                system("pause");
+                break;
+            case 5:
+                system("cls");
+                controller_removeEmployee(listaEmpleados);
+                system("pause");
+                break;
+            case 6:
+                system("cls");
+                controller_ListEmployee(listaEmpleados);
+                system("pause");
+                break;
+            case 7:
+                system("cls");
+                controller_sortEmployee(listaEmpleados);
+                system("pause");
+                break;
+            case 8:
+                system("cls");
+                controller_saveAsText("data.cvs",listaEmpleados);
+                system("pause");
+                break;
+            case 9:
+                system("cls");
+                controller_saveAsBinary("data.bin",listaEmpleados);
+                system("pause");
+                break;
+            case 10:
+                system("cls");
+                ll_clear(listaEmpleados);
+                ll_deleteLinkedList(listaEmpleados);
+                system("pause");
+                break;
+        }
+
+    }while(option != 10);
+    return 0;
+}
